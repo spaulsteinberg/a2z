85c138f444c6104e559517d14f4a136b57b3d0f1
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-02-22 11:47:15 -0600

    close #48

--- src/views/auth/ResetPasswordView.vue
+++ src/views/auth/ResetPasswordView.vue
@@ -1,23 +1,30 @@
 <template>
     <AZCard>
         <template v-slot:header>
-            <h2>Reset Password</h2>
+            <h2 class="text-center">Reset Password</h2>
         </template>
         <template v-slot:content>
-                <AZInputGroup v-model.trim="email" id="email" labelText="Email: " hasMargin/>
-                <button class="btn btn-warning auth-page-button" type="submit" v-if="!loading && !success" @click="handleResetClick">Reset Password</button>
-                <div class="spinner-container" v-else-if="loading && !success">
-                    <AZLoadingSpinner spinnerColor="primary" />
-                </div>
-                <AZFeedbackAlert 
-                    v-else-if="!loading && success" 
-                    severity="success"
-                    text="Password reset! Check you email and then log in." 
-                    />
-                <AZErrorBlock :error="error" v-if="error"/>
-                <div class="text-center">
-                    <router-link to="/login" >Back to Login Page</router-link>
-                </div>
+            <AZInputGroup 
+                v-model.trim="email" 
+                id="email" 
+                labelText="Email: "
+                :handleBlur="v.email.$touch"
+                :isInvalid="v.email.$error" 
+                hasMargin/>
+            <AZErrorBlock v-for="error of v.email?.$errors" :key="error.$uid" :error="'*' + error.$message" alignText="text-left" :isSmall="true"/>
+            <button class="btn btn-warning auth-page-button" type="submit" v-if="!loading && !success" @click="handleResetClick">Reset Password</button>
+            <div class="spinner-container" v-else-if="loading && !success">
+                <AZLoadingSpinner spinnerColor="primary" />
+            </div>
+            <AZFeedbackAlert 
+                v-else-if="!loading && success" 
+                severity="success"
+                text="Password reset! Check you email and then log in." 
+                />
+            <AZErrorBlock :error="error" v-if="error"/>
+            <div class="text-center my-1">
+                <router-link to="/login" >Back to Login Page</router-link>
+            </div>
         </template>
     </AZCard>
 </template>
@@ -30,6 +37,9 @@
     import AZErrorBlock from "../../components/utility/AZErrorBlock.vue";
     import { ref } from "vue";
     import AZFeedbackAlert from "../../components/utility/AZFeedbackAlert.vue";
+    import { useVuelidate } from '@vuelidate/core'
+    import { required, email as VuelidateEmail } from '@vuelidate/validators'
+    import { computed } from "vue";
     export default {
         name: 'ResetPasswordView',
         components: { AZCard, AZInputGroup, AZLoadingSpinner, AZErrorBlock, AZFeedbackAlert },
@@ -37,7 +47,16 @@
             const { email, store, loading, error} = useAuthState()
             const success = ref(false)
 
+            const rules = computed(() => ({
+                email: { required, VuelidateEmail }
+            }))
+
+            const v = useVuelidate(rules, { email })
+
             const handleResetClick = async () => {
+                if (v.value.$invalid) {
+                    return error.value = "Please fill out all required fields."
+                }
                 loading.value = true
                 error.value = null
                 success.value = false
@@ -53,6 +72,7 @@
 
             return {
                 email,
+                v,
                 success,
                 loading,
                 error,
