543dd3a980ac135921f215483ffdfceaeac0f60e
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-08-02 13:20:12 -0500

    close #106

---
+++ src/components/requests/RequestsHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header-container">
+        <h1 class="text-center">Requests</h1>
+        <h3 class="lead text-center">View request status, history, and choose drivers in the table.</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() { }
+}
+</script>
+
+<style scoped>
+.header-container {
+    margin-top: 2rem;
+    margin-bottom: 3rem;
+}
+</style>
